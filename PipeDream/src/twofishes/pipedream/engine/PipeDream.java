3ca6056f8fbc72c0c45dc3004cf5a784e59989b2
Elizabeth Stevens <bluenautilus2@gmail.com>
2011-12-18 22:10:08 -0600

    adding two packages

---
+++ PipeDream/src/twofishes/pipedream/engine/PipeDream.java
@@ -0,0 +1,18 @@
+package twofishes.pipedream.engine;
+
+import twofishes.pipedream.gui.*;
+
+public class PipeDream {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		MainFrame theFrame = new MainFrame();
+		theFrame.pack();
+		theFrame.setVisible(true);
+
+	}
+
+}
---
+++ PipeDream/src/twofishes/pipedream/gui/MainFrame.java
@@ -0,0 +1,11 @@
+package twofishes.pipedream.gui;
+
+import java.awt.Frame;
+
+public class MainFrame extends Frame {
+
+	
+	//add stuff
+	
+	//and things
+}

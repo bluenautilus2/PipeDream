059c8e30b4d393e36978fe2946136c44955adfd8
Scott Martin <ScottMartin574@hotmail.com>
2011-12-20 21:05:23 -0600

    Adding WindowListener so that the AWT Frame will close.

--- PipeDream/src/twofishes/pipedream/engine/PipeDream.java
+++ PipeDream/src/twofishes/pipedream/engine/PipeDream.java
@@ -1,5 +1,8 @@
 package twofishes.pipedream.engine;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import twofishes.pipedream.gui.*;
 
 public class PipeDream {
@@ -12,6 +15,11 @@ public class PipeDream {
 		MainFrame theFrame = new MainFrame();
 		theFrame.pack();
 		theFrame.setVisible(true);
+		theFrame.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent we){
+			System.exit(0);
+			}
+		});
 
 	}
 

c649ab7153832b015b61c3d984d3e0aefa594dbb
Kenneth Fazel <kf_junk@yahoo.com>
2011-12-20 18:53:03 -0600

    ok

--- PipeDream/src/twofishes/pipedream/gui/MainFrame.java
+++ PipeDream/src/twofishes/pipedream/gui/MainFrame.java
@@ -10,4 +10,16 @@ public class MainFrame extends Frame {
 	//and things
 	
 	//even more stuff
+	
+	class Stuff {
+		private Stuff stuff ;
+		public Stuff(Stuff stuff) {
+			this.stuff = stuff ;
+		}
+	}
+	
+	public Stuff stuffAndStuff(Stuff stuff) {
+		return new Stuff(stuff) ;
+	}
+	
 }

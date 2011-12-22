cdbdc9bbef836c0a8d6a5f88b605685caf44ce03
Elizabeth Stevens <bluenautilus2@gmail.com>
2011-12-21 21:54:22 -0600

    filled out class

--- PipeDream/src/twofishes/pipedream/engine/PipeQueue.java
+++ PipeDream/src/twofishes/pipedream/engine/PipeQueue.java
@@ -4,16 +4,29 @@ import java.util.ArrayList;
 
 import twofishes.pipedream.pipe.AbsPipe;
 
+/**
+ * Maintains a full queue of pipes at all times
+ * when you pop one off the queue, it adds a new one to
+ * the tail
+ * 
+ * @author bluenautilus2
+ *
+ */
+
 public class PipeQueue {
    
 	private ArrayList<AbsPipe> pipeList;
 	
 	private int FRONT_OF_QUEUE = 0;
 	
-	
+	private PipeGenerator pipeGenerator;
 	
 	public PipeQueue(int size){
+		pipeGenerator = new PipeGenerator();
 		pipeList = new ArrayList<AbsPipe>(size);
+		for(int i=0; i<size; i++){
+			this.add(pipeGenerator.getNewPipe());
+		}
 	}
 	
 	public void add(AbsPipe newPipe){
@@ -21,11 +34,20 @@ public class PipeQueue {
 	}
 	
 	public AbsPipe pop(){
+		this.add(pipeGenerator.getNewPipe());
 		return pipeList.remove(FRONT_OF_QUEUE);
 	}
 	
 	public AbsPipe peek(){
 		return pipeList.get(FRONT_OF_QUEUE);
+	}
+	
+	public AbsPipe peek(int i){
+		return pipeList.get(i);
+	}
+	
+	public int size(){
+		return pipeList.size();
 	}
 	
 	

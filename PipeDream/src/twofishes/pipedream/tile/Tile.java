950e16454b85b2558e501f2b996cb58e8f84587d
bluenautilus2 <bluenautilus2@gmail.com>
2011-12-22 15:59:00 -0600

    adding the stuff I wrote on the plane

--- PipeDream/src/twofishes/pipedream/tile/Tile.java
+++ PipeDream/src/twofishes/pipedream/tile/Tile.java
@@ -5,8 +5,21 @@ import twofishes.pipedream.pipe.*;
 
 public class Tile {
 
+	
 	private AbsPipe currentPipe;
 	
 	private TileState currentState;
+	
+	private boolean tileLocked = false;
+	
+	
+
+	public boolean isTileLocked() {
+		return tileLocked;
+	}
+
+	public void setTileLocked(boolean tileLocked) {
+		this.tileLocked = tileLocked;
+	}
 	
 }

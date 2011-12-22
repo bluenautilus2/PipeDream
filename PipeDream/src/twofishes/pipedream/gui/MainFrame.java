a2d63c6adff2de7442dc4f04dbec43a1d8a58405
Elizabeth Stevens <bluenautilus2@gmail.com>
2011-12-21 19:36:03 -0600

    fixing inappropriate comments

--- PipeDream/src/twofishes/pipedream/gui/MainFrame.java
+++ PipeDream/src/twofishes/pipedream/gui/MainFrame.java
@@ -4,24 +4,5 @@ import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
 
-	
-	//add stuff
-	
-	//and things
-	
-	//even more stuff
-	
-	class Stuff {
-		//what the hell is all this stuff
-		//Re-dick-u-lous
-		private Stuff stuff ;
-		public Stuff(Stuff stuff) {
-			this.stuff = stuff ;
-		}
-	}
-	
-	public Stuff stuffAndStuff(Stuff stuff) {
-		return new Stuff(stuff) ;
-	}
-	
+	//add some panels etc.
 }
